33c3cff4eb5466cadcb4f512276090c10ef6e19a
Baptiste <22599123+Bapteiste@users.noreply.github.com>
2018-06-14 21:32:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 bot.on('message', function (message) {
-  if (message.content === '!ping') {
-    message.reply('pong')
+  if (message.content === '/ping') {
+    message.reply('nique ta race')
+  }
+  else if (message.content === '/black-hole') {
+    message.reply('je suis un trou, salut !')
+  }
+  else if (message.content === '/baptiste') {
+    message.reply('merci de patienter')
   }
 })
 
