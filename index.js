e8c18d8fa7f432afa9a721e4869c6399f118398f
Baptiste <22599123+Bapteiste@users.noreply.github.com>
2018-06-19 11:16:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ bot.on('message', function (message) {
     message.delete(10000)
   }
   else if (message.content === '/help') {
-    my_string = '***- Commandes principales -\n***'
-    my_string += '  /message-list'
+    my_string = '```'
+    my_string += '***- Commandes principales - \n***'
+    my_string += '  /message-list \n'
+    my_string += '```'
     message.channel.send(my_string)
     message.delete(10000)
   }
